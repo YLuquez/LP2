6bf4e6e39e9fc06e60b06ce6b2d8967825153b15
YLuquez <88321907+YLuquez@users.noreply.github.com>
2022-03-14 11:27:06 -0300

    Refatorando 2.6.3

--- 2.Classes/2.6.2/figures/Ellipse.java
+++ 2.Classes/2.6.2/figures/Ellipse.java
@@ -5,29 +5,14 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
 
 public class Ellipse extends Figure {
-    int x, y;
-    int w, h;
-    //String EcorContorno, EcorFundo;
 
     public Ellipse (int x, int y, int w, int h) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        //this.EcorContorno = EcorContorno;
-        //this.EcorFundo = EcorFundo;
-    }
-
-    public void print () {
-        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+        super(x,y,w,h);
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        //g2d.setColor(Color.decode(this.EcorContorno));
-        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-        //g2d.setColor(Color.decode(this.EcorFundo));
-        //g2d.fillOval(this.x, this.y, this.w, this.h);
+        g2d.draw(new Ellipse2D.Double(x,y,w,h));
+        
     }
 }
