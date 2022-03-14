a8e0a1473dedb2f07edbc6fc7c0128664bca39fb
YLuquez <88321907+YLuquez@users.noreply.github.com>
2022-03-14 11:31:18 -0300

    Refatorando 2.6.3

--- 2.Classes/2.6.2/figures/Figure.java
+++ 2.Classes/2.6.2/figures/Figure.java
@@ -3,5 +3,14 @@ package figures;
 import java.awt.Graphics;
 
 public abstract class Figure {
+    protected int x,y;
+    protected int w, h;
+    
+    public Figure(int x, int y, int w, int h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
     public abstract void paint (Graphics g);
 }
