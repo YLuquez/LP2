f36249883ed17bfe1ce73a62f3081304e809ee50
YLuquez <88321907+YLuquez@users.noreply.github.com>
2022-03-31 19:13:41 -0300

    Update ListApp.java

--- Projeto/Projeto 1/ListApp.java
+++ Projeto/Projeto 1/ListApp.java
@@ -6,59 +6,12 @@ import java.util.Random;
 
 import figures.*;
 import handlers.*;
+import frame.*;
 
 class ListApp {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         ListFrame frame = new ListFrame();
         frame.setVisible(true);
     }
 }
 
-class ListFrame extends JFrame {
-    private static final long serialVersionUID = 1L;
-    ArrayList<Figure> figs = new ArrayList<Figure>();
-    Handlers Handler = new Handlers();
-    Random rand = new Random();
-
-    ListFrame () {
-        this.addWindowListener (
-            new WindowAdapter() {
-                public void windowClosing (WindowEvent e) {
-                    System.exit(0);
-                }
-            }
-        );
-        
-        this.addMouseListener (
-            new MouseAdapter() {
-                public void mouseMoved(MouseEvent evt) {
-
-                }
-            }
-
-        );
-
-        this.addKeyListener (
-            new KeyAdapter() {
-                public void keyPressed(KeyEvent evt) {
-                    Figure fig = Handler.HandleWithKey(evt);
-
-                    if (fig != null) {
-                        figs.add(fig);
-                    }
-                    repaint();
-                }
-            }
-        );
-
-        this.setTitle("Projeto 1/2");
-        this.setSize(500, 500);
-    }
-
-    public void paint (Graphics g) {
-        super.paint(g);
-        for (Figure fig: this.figs) {
-            fig.paint(g);
-        }
-    }
-}
